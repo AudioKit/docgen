9a6ea925b321a01dee8b81670b1568182b708fa7
Brian Drelling <brian@drelling.com>
2022-08-19 19:47:29 -0500

    Bumped docgen

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "AudioKitDocs",
     dependencies: [
-        .package(url: "https://github.com/bdrelling/docgen", from: "0.1.6"),
+        .package(url: "https://github.com/bdrelling/docgen", from: "0.1.7"),
         // Add packages you wish to generate documentation for here.
         .package(url: "https://github.com/AudioKit/AudioKit", from: "5.4.1"),
         .package(url: "https://github.com/AudioKit/PianoRoll", from: "1.0.1"),
