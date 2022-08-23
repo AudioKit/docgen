72f8584fea1361ff7ff89f08f61155fb7e208d6a
Brian Drelling <brian@drelling.com>
2022-08-22 20:09:34 -0500

    Updated unpack script

--- scripts/unpack.sh
+++ scripts/unpack.sh
@@ -3,4 +3,6 @@
 # Define the output directory.
 output_directory=${1-'.'}
 
+# Unpacks the .zip containing the .doccarchive files.
+# The output directory will include the archive directory (eg. "docs/") by default.
 unzip ./docs/docs.zip -d $output_directory
